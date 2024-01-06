e547a8e43ca211e459dd33993ed76c6a5c4ff5ae
Dwivedi Developer <dwivedi.abhishek2023@gmail.com>
2024-01-07 01:10:22 +0530

    path updated

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
    currFolder = folder;
-    let a = await fetch(`https://dwivedi-developer.github.io/MeloHub/${currFolder}/`);
+    let a = await fetch(`http://127.0.0.1:5500/${currFolder}/`);
     let response = await a.text();
 
     let div = document.createElement("div");
